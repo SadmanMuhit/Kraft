2487d5349b9b0f04d2ebe6eadc9fcb9f3844d84e
sadmandev01 <sadmanmuhit10@gmail.com>
2024-09-28 16:00:44 +0600

    change

--- src/Page/Blog.jsx
+++ src/Page/Blog.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from '../component/home/Navbar'
 import Blogitems from '../component/Blog/Blogitems'
 import Banner from '../component/Blog/Banner'
+import Footer from '../component/home/Footer'
 const Blog = () => {
   return (
     <>
       <Navbar/>
       <Banner/>
       <Blogitems/>
+      <Footer/>
     </>
   )
 }
--- src/component/Blog/Blogitems.jsx
+++ src/component/Blog/Blogitems.jsx
@@ -3,19 +3,66 @@ import React from 'react'
 const Blogitems = () => {
   return (
     <section className='pt-10 pb-20'>
-        <div className="container gap-12 justify-center flex">
-            <div className='w-full max-w-6xl'>
-                <img src="/Blog.png" alt="Blog" />
-            </div>
-            <div className='flex flex-col justify-center'>
-              <h2 className='font-roboto font-medium text-3xl text-third'>Balance is important in design</h2>
-              <ul>
-                <li><a href="#"></a></li>
-                <li><a href="#"></a></li>
-                <li><a href="#"></a></li>
-              </ul>
-            </div>
+      <div className="container flex gap-[50px] mb-[60px]">
+        <div>
+          <img src="/Blog.png" alt="" />
         </div>
+        <div className='ml-12 mt-[60px]'>
+          <h2 className='text-third font-roboto font-medium text-3xl'>Balance is important in design</h2>
+          <h3 className='text-third font-roboto font-medium text-sm pt-2 pb-5'>By Caliberthemes / In Inspirational / On 22 Sep 2019</h3>
+          <p className='text-secendary pb-7 max-w-sm'>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+commodo ligula eget dolor. Aenean massa. Cum sociis Theme
+natoque penatibus et magnis.</p>
+            <div className='mt-7'>
+              <button className='broder-solid border-2 border-black px-4 py-2'>Read More</button>
+            </div>
+        </div> 
+      </div>
+      <div className="container flex gap-[50px] mb-[60px]">
+        <div>
+          <img src="/Blog2.png" alt="" />
+        </div>
+        <div className='ml-12 mt-[60px]'>
+          <h2 className='text-third font-roboto font-medium text-3xl'>Balance is important in design</h2>
+          <h3 className='text-third font-roboto font-medium text-sm pt-2 pb-5'>By Caliberthemes / In Inspirational / On 22 Sep 2019</h3>
+          <p className='text-secendary pb-7 max-w-sm'>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+commodo ligula eget dolor. Aenean massa. Cum sociis Theme
+natoque penatibus et magnis.</p>
+            <div className='mt-7'>
+              <button className='broder-solid border-2 border-black px-4 py-2'>Read More</button>
+            </div>
+        </div> 
+      </div>
+      <div className="container flex gap-[50px] mb-[60px]">
+        <div>
+          <img src="/Blog3.png" alt="" />
+        </div>
+        <div className='ml-12 mt-[60px]'>
+          <h2 className='text-third font-roboto font-medium text-3xl'>Balance is important in design</h2>
+          <h3 className='text-third font-roboto font-medium text-sm pt-2 pb-5'>By Caliberthemes / In Inspirational / On 22 Sep 2019</h3>
+          <p className='text-secendary pb-7 max-w-sm'>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+commodo ligula eget dolor. Aenean massa. Cum sociis Theme
+natoque penatibus et magnis.</p>
+            <div className='mt-7'>
+              <button className='broder-solid border-2 border-black px-4 py-2'>Read More</button>
+            </div>
+        </div> 
+      </div>
+      <div className="container flex gap-[50px] mb-[60px]">
+        <div>
+          <img src="/Blog4.png" alt="" />
+        </div>
+        <div className='ml-12 mt-[60px]'>
+          <h2 className='text-third font-roboto font-medium text-3xl'>Balance is important in design</h2>
+          <h3 className='text-third font-roboto font-medium text-sm pt-2 pb-5'>By Caliberthemes / In Inspirational / On 22 Sep 2019</h3>
+          <p className='text-secendary pb-7 max-w-sm'>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+commodo ligula eget dolor. Aenean massa. Cum sociis Theme
+natoque penatibus et magnis.</p>
+            <div className='mt-7'>
+              <button className='broder-solid border-2 border-black px-4 py-2'>Read More</button>
+            </div>
+        </div> 
+      </div>
     </section>
   )
 }
