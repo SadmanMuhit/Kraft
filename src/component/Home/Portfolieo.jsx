d18779a6e9daa8d463fdd1e409c15147390fa914
SadmanMuhit <sadmanmuhit@gmail.com>
2025-05-07 00:10:21 +0600

    portfolio

--- src/component/Home/Portfolieo.jsx
+++ src/component/Home/Portfolieo.jsx
@@ -19,15 +19,61 @@ const Portfolieo = () => {
                 </li>
             </ul>
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
-                <img src="/Container.png" alt="" className="w-full"/>
-                <img src="/Container.png" alt="" className="w-full"/>
-                <img src="/Container.png" alt="" className="w-full"/>
-                <img src="/Container.png" alt="" className="w-full"/>
-                <img src="/Container.png" alt="" className="w-full"/>
-                <img src="/Container.png" alt="" className="w-full"/>        
-                <img src="/Container.png" alt="" className="w-full"/>
-                <img src="/Container.png" alt="" className="w-full"/>
-                <img src="/Container.png" alt="" className="w-full"/>
+               <div className='relative overflow-hidden group'>
+               <img src="/Container.png" alt="" className="w-full object-cover"/>
+               <div className='absolute inset-0 bg-black bg-opacity-80 opacity-0 group-hover:opacity-100 flex flex-col justify-center items-center text-white transition-opacity duration-300'>
+                <p>lightbox</p>
+                <p>lightbox</p>
+               </div>
+               </div><div className='relative overflow-hidden group'>
+               <img src="/Container.png" alt="" className="w-full object-cover"/>
+               <div className='absolute inset-0 bg-black bg-opacity-80 opacity-0 group-hover:opacity-100 flex flex-col justify-center items-center text-white transition-opacity duration-300'>
+                <p>lightbox</p>
+                <p>lightbox</p>
+               </div>
+               </div><div className='relative overflow-hidden group'>
+               <img src="/Container.png" alt="" className="w-full object-cover"/>
+               <div className='absolute inset-0 bg-black bg-opacity-80 opacity-0 group-hover:opacity-100 flex flex-col justify-center items-center text-white transition-opacity duration-300'>
+                <p>lightbox</p>
+                <p>lightbox</p>
+               </div>
+               </div><div className='relative overflow-hidden group'>
+               <img src="/Container.png" alt="" className="w-full object-cover"/>
+               <div className='absolute inset-0 bg-black bg-opacity-80 opacity-0 group-hover:opacity-100 flex flex-col justify-center items-center text-white transition-opacity duration-300'>
+                <p>lightbox</p>
+                <p>lightbox</p>
+               </div>
+               </div><div className='relative overflow-hidden group'>
+               <img src="/Container.png" alt="" className="w-full object-cover"/>
+               <div className='absolute inset-0 bg-black bg-opacity-80 opacity-0 group-hover:opacity-100 flex flex-col justify-center items-center text-white transition-opacity duration-300'>
+                <p>lightbox</p>
+                <p>lightbox</p>
+               </div>
+               </div><div className='relative overflow-hidden group'>
+               <img src="/Container.png" alt="" className="w-full object-cover"/>
+               <div className='absolute inset-0 bg-black bg-opacity-80 opacity-0 group-hover:opacity-100 flex flex-col justify-center items-center text-white transition-opacity duration-300'>
+                <p>lightbox</p>
+                <p>lightbox</p>
+               </div>
+               </div><div className='relative overflow-hidden group'>
+               <img src="/Container.png" alt="" className="w-full object-cover"/>
+               <div className='absolute inset-0 bg-black bg-opacity-80 opacity-0 group-hover:opacity-100 flex flex-col justify-center items-center text-white transition-opacity duration-300'>
+                <p>lightbox</p>
+                <p>lightbox</p>
+               </div>
+               </div><div className='relative overflow-hidden group'>
+               <img src="/Container.png" alt="" className="w-full object-cover"/>
+               <div className='absolute inset-0 bg-black bg-opacity-80 opacity-0 group-hover:opacity-100 flex flex-col justify-center items-center text-white transition-opacity duration-300'>
+                <p>lightbox</p>
+                <p>lightbox</p>
+               </div>
+               </div><div className='relative overflow-hidden group'>
+               <img src="/Container.png" alt="" className="w-full object-cover"/>
+               <div className='absolute inset-0 bg-black bg-opacity-80 opacity-0 group-hover:opacity-100 flex flex-col justify-center items-center text-white transition-opacity duration-300'>
+                <p>lightbox</p>
+                <p>lightbox</p>
+               </div>
+               </div>
             </div>
         </div>
     </section> 
